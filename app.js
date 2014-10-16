c17bd5a46e264d1e9b56829ec32bb70c5426723d
brianhenning <brian.henning.m@gmail.com>
2014-10-15 21:31:45 -0400

    Layout Complete
    Beginning Javascript

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	/*---Page Load--*/
+	$('#home').show();
+	$('#questionone').hide();
+	$('#oneresult').hide();
+	$('#questiontwo').hide();
+	$('#tworesult').hide();
+	$('#questionthree').hide();
+	$('#threeresult').hide();
+	$('#questionfour').hide();
+	$('#fourresult').hide();
+	$('#results').hide();
+
+	/*--Start Quiz--*/
+	
+
+
+
+});
